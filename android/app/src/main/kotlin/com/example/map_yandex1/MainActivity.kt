5aca9375b5c2bcb8c43df66414874086e5952c10
Jasmina06 <abduhakimovayasmina14@gmail.com>
2025-02-05 14:44:43 +0500

    something

--- android/app/src/main/kotlin/com/example/map_yandex1/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/map_yandex1/MainActivity.kt
@@ -1,15 +1,13 @@
-// MainApplication.java
 package com.example.map_yandex1
 
 import android.app.Application
 import com.yandex.mapkit.MapKitFactory
 
 class MainApplication : Application() {
-    @Override
-    fun onCreate() {
+    override fun onCreate() {
         super.onCreate()
-        
-        // Инициализация Yandex MapKit с вашим API ключом
+
+
         MapKitFactory.setApiKey("0c25ab95-5dd3-4d59-981a-d74d66a8158c")
     }
 }
